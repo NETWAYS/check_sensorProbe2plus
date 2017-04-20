408fddaa6deb7400d8a6dc91eee5e5a5069369d5
Noah Hilverling <noah.hilverling@netways.de>
2017-04-20 11:46:23 +0200

    Add copyright and license

--- check_sensorProbe2plus.py
+++ check_sensorProbe2plus.py
@@ -1,3 +1,24 @@
+# ------------------------------------------------------------------------------
+# check_sensorProbe2plus.py - A check plugin for AKCP SensorProbe2+.
+# Copyright (C) 2017  NETWAYS GmbH, www.netways.de
+# Authors: Noah Hilverling <noah.hilverling@netways.de>, Jennifer Mourek <jennifer.mourek@netways.de>
+# Version: 1.0
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# ------------------------------------------------------------------------------
+
 from easysnmp import Session
 from enum import Enum, IntEnum
 import sys
